1c56022fff2570fb51d8d672592895470a5f27ee
David Barratt <david@davidwbarratt.com>
2017-06-24 11:22:23 -0400

    Fix coveralls

--- tests/ParameterBagTest.php
+++ tests/ParameterBagTest.php
@@ -18,4 +18,53 @@ class ParameterBagTest extends TestCase
         $bag = new ParameterBag($data);
         $this->assertEquals($data, $bag->all());
     }
+
+    public function testKeys()
+    {
+        $data = [
+            'key' => 'value',
+        ];
+        $bag = new ParameterBag($data);
+        $this->assertEquals(['key'], $bag->keys());
+    }
+
+    public function testReplace()
+    {
+        $data = [
+            'key' => 'value',
+        ];
+        $bag = new ParameterBag($data);
+
+        $newData = [
+            'newKey' => 'newValue',
+        ];
+        $bag->replace($newData);
+
+        $this->assertEquals($newData, $bag->all());
+    }
+
+    public function testAdd()
+    {
+        $data = [
+            'key' => 'value',
+        ];
+        $bag = new ParameterBag($data);
+
+        $newData = [
+            'newKey' => 'newValue',
+        ];
+        $bag->add($newData);
+
+        $this->assertEquals(array_replace($data, $newData), $bag->all());
+    }
+
+    public function testGet()
+    {
+        $data = [
+            'key' => 'value',
+        ];
+        $bag = new ParameterBag($data);
+        $this->assertEquals('value', $bag->get('key'));
+        $this->assertEquals('default', $bag->get('nokey', 'default'));
+    }
 }
