35fc9e82f2c75862717d53cee3ac68b12171c22e
David Barratt <david@davidwbarratt.com>
2017-06-24 11:51:48 -0400

    Add more tests

--- tests/ParameterBagTest.php
+++ tests/ParameterBagTest.php
@@ -104,4 +104,110 @@ class ParameterBagTest extends TestCase
 
         $this->assertFalse($bag->has('key'));
     }
+
+    public function testGetInit()
+    {
+        $data = [
+            'number' => '12345',
+            'bool' => true,
+            'int' => 12345,
+        ];
+
+        $bag = new ParameterBag($data);
+
+        $this->assertSame(12345, $bag->getInt('int', 0));
+        $this->assertSame(0, $bag->getInt('number', 0));
+        $this->assertSame(0, $bag->getInt('bool', 0));
+        $this->assertSame(0, $bag->getInt('nokey', 0));
+    }
+
+    public function testGetBoolean()
+    {
+        $data = [
+            'string' => 'true',
+            'int' => 1,
+            'bool' => true,
+        ];
+
+        $bag = new ParameterBag($data);
+
+        $this->assertSame(true, $bag->getBoolean('bool', false));
+        $this->assertSame(false, $bag->getBoolean('int', false));
+        $this->assertSame(false, $bag->getBoolean('number', false));
+        $this->assertSame(false, $bag->getBoolean('nokey', false));
+    }
+
+    public function testGetString()
+    {
+        $data = [
+            'string' => 'abc',
+            'int' => 1,
+            'bool' => true,
+        ];
+
+        $bag = new ParameterBag($data);
+
+        $this->assertSame('abc', $bag->getString('string', 'xyz'));
+        $this->assertSame('xyz', $bag->getString('int', 'xyz'));
+        $this->assertSame('xyz', $bag->getString('number', 'xyz'));
+        $this->assertSame('xyz', $bag->getString('nokey', 'xyz'));
+    }
+
+    public function testGetArray()
+    {
+        $data = [
+            'string' => 'array',
+            'int' => 1,
+            'bool' => true,
+            'array' => [1,2,3],
+        ];
+
+        $bag = new ParameterBag($data);
+
+        $this->assertSame([1,2,3], $bag->getArray('array', [4,5,6]));
+        $this->assertSame([4,5,6], $bag->getArray('string', [4,5,6]));
+        $this->assertSame([4,5,6], $bag->getArray('int', [4,5,6]));
+        $this->assertSame([4,5,6], $bag->getArray('bool', [4,5,6]));
+        $this->assertSame([4,5,6], $bag->getArray('nokey', [4,5,6]));
+    }
+
+    public function testGetUuid()
+    {
+        $real = '5d110ca4-58f4-11e7-97a7-a45e60d54511';
+        $data = [
+            'uuid' => $real,
+            'string' => 'abc',
+        ];
+
+        $bag = new ParameterBag($data);
+
+        $default = '75568f1e-58f4-11e7-97a7-a45e60d54511';
+        $this->assertSame($real, $bag->getUuid('uuid', $default));
+        $this->assertSame($default, $bag->getUuid('string', $default));
+        $this->assertSame($default, $bag->getUuid('nokey', $default));
+    }
+
+    public function testGetIterator()
+    {
+        $data = [
+            'key' => 'value',
+        ];
+        $bag = new ParameterBag($data);
+
+        $iterator = $bag->getIterator();
+
+        $this->assertInstanceOf(\ArrayIterator::class, $iterator);
+    }
+
+    public function testCount()
+    {
+        $data = [
+            'key' => 'value',
+        ];
+        $bag = new ParameterBag($data);
+        $this->assertEquals(1, $bag->count());
+
+        $bag = new ParameterBag();
+        $this->assertEquals(0, $bag->count());
+    }
 }
